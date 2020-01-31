b5390e7b2f2d0bbd3d440e26e23b2e7230cd25a3
Dimacauchi9 <56259917+Dimacauchi9@users.noreply.github.com>
2020-01-31 16:49:41 +0100

    Update index.php

--- index.php
+++ index.php
@@ -77,6 +77,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="">
   <meta name="author" content="">
+    
 
   <title>Creative Investment</title>
 
@@ -119,7 +120,7 @@
     <div class="container h-100">
       <div class="row h-100 align-items-center justify-content-center text-center">
         <div class="col-lg-10 align-self-end">
-          <h1 class="text-uppercase text-white font-weight-bold">Where Invester and entraprenur create great things togenter</h1>
+          <h1 class="text-uppercase text-white font-weight-bold">Where Investors and Entrapreneurs create great things together</h1>
           <hr class="divider my-4">
         </div>
         <div class="col-lg-8 align-self-baseline">
