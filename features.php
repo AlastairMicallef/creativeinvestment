d7cc7050c696b8265bdc62b020a1c8f8e3e18b9c
Clint <58631855+buzu99@users.noreply.github.com>
2020-02-01 09:29:18 +0100

    Add files via upload

--- features.php
+++ features.php
@@ -1,24 +1,104 @@
+
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Creative Investment</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
-    <script src="main.js"></script>
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Creative Investment</title>
+
+  <!-- Font Awesome Icons -->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet">
+  <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic' rel='stylesheet' type='text/css'>
+
+  <!-- Plugin CSS -->
+  <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+
+  <!-- Theme CSS - Includes Bootstrap -->
+  <link href="css/creative.min.css " rel="stylesheet">
+
 </head>
+
+<body id="page-top">
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">Creative Investment</a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto my-2 my-lg-0">
+           <a class="nav-item nav-link active" href="index.php">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-item nav-link " href="features.php">Features</a>
+            <a class="nav-item nav-link" href="pricing.php">Pricing</a>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Masthead -->
+  <header class="masthead">
+    <div class="container h-100">
+      <div class="row h-100 align-items-center justify-content-center text-center">
+        <div class="col-lg-10 align-self-end">
+          <h1 class="text-uppercase text-white font-weight-bold">Features</h1>
+          <hr class="divider my-4">
+        </div>
+      </div>
+    </div>
+  </header>
 <body>
     <div class="jumbotron">
-        <h1 class="display-4">Creative Investment</h1>
-        <p class="lead">Where Investors and Entrapreneurs create great things together</p>
+    <div class="container h-100">
+      <div class="row h-100 align-items-center justify-content-center text-center">
+        <div class="col-lg-10 align-self-end">
+
+
         <hr class="my-4">
-        <p>Invest beyond the ordinary</p>
-        <a class="btn btn-primary btn-lg" href="register.php" role="button">Become a member</a>
+        <h1 class="display-5">Some of our features include: </h1>
+
+        <p><img src="https://img-aws.ehowcdn.com/340x221p/photos.demandstudios.com/getty/article/244/85/200249342-001.jpg" style='width:auto; height: 600px' class="img-fluid" alt="Responsive image"></p>
+       
+        <h1 class="display-5">Communication</h1>
+        <p>In order to form an alliance between the entrepreneur and the investor there must be some type of Communication
+        where both of them are able to communicate iwth each other about the details needed to form the alliance 
+        in order for the investor to invest and also the entrepreneur to start the business, therefore this website
+        offers the service of communication for both of the matching entrepreneur and the investor in order to be able 
+        to communicate with eachother.   </p>
+
+        <p><img src="https://nashkassam.com/wp-content/uploads/2018/02/make-the-tough-choice.jpg" style='width:auto; height: 600px' class="img-fluid" alt="Responsive image"></p>
+
+        <h1 class="display-5">Choice</h1>
+        <p>For an entrepreneur to startup a business and an ivestor to make an investment, they must choose 
+        the right candidate according to their required specifications including the type of market they are willing
+        to invest in or the type of market they are going to make part of when they startup their business,
+        while also the investor may want to invest a specific amount of money in order to stay within their 
+        budget. While an entrepremeur may want to select the investor who is willing to pay the required amount 
+        for the entrepreneur to start his business. Therefore thiss website provides a variety for the investor
+        or the entrepreneur to choose his required partner.  </p>
+
+
+        <p><img src="https://magnifyingglassesinfo.com/wp-content/uploads/2015/12/814-mW0J8TL._SL1500_.jpg" style='width:auto; height: 600px' class="img-fluid" alt="Responsive image"></p>
+        <h1 class="display-5">Important details</h1>
+        <p>Investors need details when they are trying to invest in a business uch as the required budget, the amount of 
+        shares a business is willing to give away, the type of market it forms a part . Also entrepreneurs will 
+        need details of what type of market the investor would like to invest as certsin investors might have connections
+        with a certain market allowing the entrepreneur to make those connections developing his business, his budget
+        and a lot of othr fine details needed in order to make a business happen before one makes a choice, therefore
+        on this website when someone is choosing the right partner, there will be all the details needed for them to 
+        choose
+        </p>
+
+ <a class="btn btn-primary btn-lg" href="register.php" role="button">Become a member</a>
     </div>
 
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
