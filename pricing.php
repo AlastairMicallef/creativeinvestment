17fc4f419370d529b3a81de2e067d5190f35421d
Dimacauchi9 <56259917+Dimacauchi9@users.noreply.github.com>
2020-02-02 11:46:55 +0100

    2

--- pricing.php
+++ pricing.php
@@ -11,16 +11,43 @@
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+      <!-- Theme CSS - Includes Bootstrap -->
+  <link href="css/creative.css " rel="stylesheet">
 </head>
 <body>
-    
-    <div class="jumbotron" style="">
-        <h1 class="display-4">Creative Investment</h1>
-        <p class="lead">Where Invester and Entrepreneur create great things togenter</p>
-        <hr class="my-4">
-        <p>Invest beyond the ordinary</p>
-        <a class="btn btn-primary btn-lg" href="register.php" role="button">Become a member</a>
+<nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">Creative Investment</a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto my-2 my-lg-0">
+           <a class="nav-item nav-link active" href="index.php">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-item nav-link " href="features.php">Features</a>
+            <a class="nav-item nav-link" href="pricing.php">Pricing</a>
+        </ul>
+      </div>
     </div>
+  </nav>
+  <header class="masthead">
+    <div class="container h-100">
+      <div class="row h-100 align-items-center justify-content-center text-center">
+        <div class="col-lg-10 align-self-end">
+          <h1 class="text-uppercase text-white font-weight-bold">Where Investors and Entrapreneurs create great things together</h1>
+          <hr class="divider my-4">
+        </div>
+        
+        <div class="col-lg-8 align-self-baseline">
+          <p class="text-white-75 font-weight-light mb-5">Invest beyond the ordinary</p>
+          <a class="btn btn-primary btn-lg" href="register.php" role="button">Pricing</a>
+        </div>
+      </div>
+    </div>
+  </header>
+
+<!--    
 
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -33,7 +60,7 @@
             <a class="nav-item nav-link" href="pricing.php">Pricing</a>
             </div>
         </div>
-    </nav>
+    </nav> -->
 
     <div class="row">
         <div class="col-sm-6">
