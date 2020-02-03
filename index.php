9f442e215b9e40b734b52024a51311d8cbea9207
Dimacauchi9 <56259917+Dimacauchi9@users.noreply.github.com>
2020-02-03 19:37:12 +0100

    Update index.php

--- index.php
+++ index.php
@@ -64,7 +64,7 @@
     <div class="container h-100">
       <div class="row h-100 align-items-center justify-content-center text-center">
         <div class="col-lg-10 align-self-end">
-          <h1 class="text-uppercase text-white font-weight-bold">Where Investors and Entrepenuers create great things together</h1>
+          <h1 class="text-uppercase text-white font-weight-bold">Where Investors and Entreprenuers create great things together</h1>
         </div>
         <div class="col-lg-8 align-self-baseline">
           <p class="text-white-75 font-weight-light mb-5">Invest beyond the ordinary</p>
